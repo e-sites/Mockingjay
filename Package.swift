ab764366c9eefa41104e6531bfef75c9654c9d3d
Bas van Kuijck <bas@e-sites.nl>
2019-10-10 10:58:07 +0200

    e-sites repo

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         )
     ],
     dependencies: [
-      .package(url: "https://github.com/basvankuijck/URITemplate.swift.git", .branch("master"))
+      .package(url: "https://github.com/e-sites/URITemplate.swift.git", .branch("master"))
     ],
     targets: [
         .target(
