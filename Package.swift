0bcf9de1a6dbfa54b698f5038dcae368f33fd569
Bas van Kuijck <bas@e-sites.nl>
2019-11-04 15:23:39 +0100

    Updated MockingJay

--- Package.swift
+++ Package.swift
@@ -12,6 +12,10 @@ let package = Package(
         .library(
             name: "Mockingjay",
             targets: ["Mockingjay"]
+        ),
+        .library(
+            name: "MockingjayXCTest",
+            targets: ["MockingjayXCTest"]
         )
     ],
     dependencies: [
@@ -21,7 +25,12 @@ let package = Package(
         .target(
             name: "Mockingjay",
             dependencies: [ "URITemplate" ],
-            path: "Sources"
+            path: "Sources/Core"
+        ),
+        .target(
+            name: "MockingjayXCTest",
+            dependencies: [ "Mockingjay" ],
+            path: "Sources/XCTest"
         ),
         .testTarget(
           name: "MockingjayTests",
